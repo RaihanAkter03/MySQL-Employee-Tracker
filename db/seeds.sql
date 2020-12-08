6e78b7101eb4be2d3743b7f93335f2828933ae6f
RaihanAkter03 <raihanakter26@gmail.com>
2020-12-08 17:52:06 -0600

    changes to sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,15 +1,8 @@
-USE employee_db;
+INSERT INTO department(department_name)
+VALUES("Engineering"), ("Sales"), ("Finance"), ("Legal"), ("Marketing"),("Software Development");
 
-INSERT INTO department (name) VALUES ("Human Resources");
-INSERT INTO department (name) VALUES ("Marketing");
-INSERT INTO department (name) VALUES ("Information Technology");
-INSERT INTO department (name) VALUES ("Corporate");
+INSERT INTO role(title, salary, department_id)
+VALUES("Engineer", 85000, 1), ("Senior Engineer", 125000, 1), ("CEO", 350000, 3), ("Chief", 200000, 4);
 
-INSERT INTO role (title, salary, department_id) VALUES ("Analyst",80, 3);
-INSERT INTO role (title, salary, department_id) VALUES ("Communications Associate",90, 2);
-INSERT INTO role (title, salary, department_id) VALUES ("Social Media Manager",100, 2);
-INSERT INTO role (title, salary, department_id) VALUES ("Director",120, 1);
-INSERT INTO role (title, salary, department_id) VALUES ("Developer",180, 4);
-
-INSERT INTO employee (first_name, last_name, role_id) VALUES ("Raihan", "Akter", 2);
-INSERT INTO employee (first_name, last_name, role_id) VALUES ("Abir", "Rifat", 1);
+INSERT INTO employee(first_name, last_name, role_id, manager_id)
+VALUES ('Raihan', 'Akter', 1, 2), ('Rifat', 'Arif', 1, null), ('Jhon', 'David', 1, 2), ('Chanel', 'Jones', 2, 2), ('Blueberry', 'Park', 4, null);
